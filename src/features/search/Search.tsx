656ed22047be10d5b3dbb425ebc79e543641443d
Henry Catalini Smith <henry@catalinismith.com>
2021-02-09 19:37:49 +0100

    set up empty search feature

---
+++ src/features/search/Search.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import {
+  // decrement,
+  // increment,
+  // incrementByAmount,
+  // incrementAsync,
+  // selectCount,
+// } from "./searchSlice";
+import styles from "./Search.module.scss";
+
+export function Search() {
+  // const count = useSelector(selectCount);
+  // const dispatch = useDispatch();
+  // const [incrementAmount, setIncrementAmount] = useState("2");
+
+  return (
+    <div className={styles.search}>
+      search
+    </div>
+  );
+}
---
+++ src/features/search/searchSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+//import { AppThunk, RootState } from "../../app/store";
+import { RootState } from "../../app/store";
+
+interface SearchState {
+  query: string;
+}
+
+const initialState: SearchState = {
+  query: "",
+};
+
+export const searchSlice = createSlice({
+  name: "search",
+  initialState,
+  reducers: {
+    query: (state, action: PayloadAction<string>) => {
+      state.query = action.payload;
+    },
+  },
+});
+
+export const { query } = searchSlice.actions;
+
+export const selectSearchQuery = (state: RootState) => state.search.query;
+
+export default searchSlice.reducer;
+

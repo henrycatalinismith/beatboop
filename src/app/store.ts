cbde363c3176fc6c2064558a87aa9a16b4f85d78
Henry Catalini Smith <henry@catalinismith.com>
2021-02-09 19:58:39 +0100

    remove boilerplate

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import searchReducer from "../features/search/searchSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     search: searchReducer,
   },
 });

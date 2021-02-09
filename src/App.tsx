d796b5fc8e8e07297ae34a4849ff3466e1fefa13
Henry Catalini Smith <henry@catalinismith.com>
2021-02-09 19:29:37 +0100

    install sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
-import "./App.css";
+import styles from "./App.module.scss";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
